ce718248acc216308c47840a759ce83a4b4165c0
ReyesMagosSoft.co <reyesmagossoft.co@gmail.com>
2014-07-20 02:39:41 -0500

    zx<x
    z<x<zx

---
+++ src/co/gov/fna/okeda/interfaces/IFactoryPuntoAtencion.java
@@ -0,0 +1,19 @@
+package co.gov.fna.okeda.interfaces;
+
+
+
+import org.json.JSONArray;
+
+import co.gov.fna.okeda.modelo.entidades.PuntoAtencion;
+
+import java.util.List;
+
+/**
+ * Created by Alexis-PC on 19/07/2014.
+ */
+public interface IFactoryPuntoAtencion {
+
+    List<PuntoAtencion> getPuntoAtencionRest();
+
+    public void fillPuntoAtencion(JSONArray arreglo, String[] arrayPropertiesNames);
+}
